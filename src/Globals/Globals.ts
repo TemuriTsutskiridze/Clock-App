7acd9a6f70d32255fb672ac2b17ae71e90d06f2e
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-07-22 19:27:03 +0400

    move background-images in public directory and change their url

--- src/Globals/Globals.ts
+++ src/Globals/Globals.ts
@@ -18,10 +18,10 @@ const GlobalStyles = createGlobalStyle<{ currentTime: number }>`
       background-color: rgba(0, 0, 0, 0.4);
       background-image: url(${props?.currentTime >= 6 &&
       props?.currentTime <= 20
-        ? "./assets/mobile/bg-image-daytime.jpg"
+        ? "/mobile/bg-image-daytime.jpg"
         : (props.currentTime > 20 && props.currentTime <= 24) ||
           (props.currentTime >= 0 && props.currentTime <= 5)
-        ? "./assets/mobile/bg-image-nighttime.jpg"
+        ? "/mobile/bg-image-nighttime.jpg"
         : null});
       background-blend-mode: multiply;
       background-repeat: no-repeat;
@@ -35,10 +35,10 @@ const GlobalStyles = createGlobalStyle<{ currentTime: number }>`
       body {
         background-image: url(${props?.currentTime >= 6 &&
         props?.currentTime <= 20
-          ? "./assets/tablet/bg-image-daytime.jpg"
+          ? "/tablet/bg-image-daytime.jpg"
           : (props.currentTime > 20 && props.currentTime <= 24) ||
             (props.currentTime >= 0 && props.currentTime <= 5)
-          ? "./assets/tablet/bg-image-nighttime.jpg"
+          ? "/tablet/bg-image-nighttime.jpg"
           : null});
 
         padding: 0 6.4rem;
@@ -49,10 +49,10 @@ const GlobalStyles = createGlobalStyle<{ currentTime: number }>`
       body {
         background-image: url(${props?.currentTime >= 6 &&
         props?.currentTime <= 20
-          ? "./assets/desktop/bg-image-daytime.jpg"
+          ? "/desktop/bg-image-daytime.jpg"
           : (props.currentTime > 20 && props.currentTime <= 24) ||
             (props.currentTime >= 0 && props.currentTime <= 5)
-          ? "./assets/desktop/bg-image-nighttime.jpg"
+          ? "/desktop/bg-image-nighttime.jpg"
           : null});
       }
 
