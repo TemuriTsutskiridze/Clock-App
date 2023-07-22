c40d7eef614e368aafcc993dd0318d5c5dd2aae5
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-07-22 18:55:05 +0400

    remove helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Quotes from "./components/Quotes";
 import Clock from "./components/Clock";
-import GlobalStyles from "./Globals";
-import { Helmet } from "react-helmet";
+import GlobalStyles from "./Globals/Globals";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -14,14 +13,6 @@ function App() {
   }, []);
   return (
     <>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <GlobalStyles currentTime={currentTime} />
       <Clock>
         <Quotes />
